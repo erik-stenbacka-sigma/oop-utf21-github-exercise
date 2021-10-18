29b224b6c5953cbb7d383b8be22c3eeaeebaf6ec
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-10-18 09:43:12 +0200

    Add project files.

---
+++ GithubRepository1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubRepository1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
