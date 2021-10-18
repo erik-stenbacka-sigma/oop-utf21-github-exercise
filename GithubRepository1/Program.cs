afb71c2729dbb0c6ad9f1f395936a770ef97f8d4
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-10-18 09:44:18 +0200

    Updated message

--- GithubRepository1/Program.cs
+++ GithubRepository1/Program.cs
@@ -1,12 +1,3 @@
 ﻿using System;
 
-namespace GithubRepository1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Console.WriteLine("Hello Github!");
